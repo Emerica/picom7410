c496661e3491a217c158e047f8fc154d93233744
MrBoogs <blendz@shaw.ca>
2015-03-10 11:51:15 -0600

    More updates

--- picom.py
+++ picom.py
@@ -7,42 +7,72 @@ ser = serial.Serial('/dev/ttyUSB0', 19200)
 def ic7410_get_frequency():
     s = ser.write("\xfe\xfe\x80\x00\x03\xfd")
 
+def ic7410_show_mode(byte):
+    if byte == "\x00":
+        print "Mode: LSB"
+    elif byte == "\x01":
+        print "Mode: USB"
+    elif byte == "\x02":
+        print "Mode: AM"
+    elif byte == "\x03":
+        print "Mode: CW"
+    elif byte == "\x04":
+        print "Mode: RTTY"
+    elif byte == "\x05":
+        print "Mode: FM"
+    elif byte == "\x06":
+        print "Mode: Wide FM"
+    elif byte == "\x07":
+        print "Mode: CW-R"
+    elif byte == "\x08":
+        print "Mode: RTTY-R"
 
-def read_7410():
+def ic7410_show_filter(byte):
+    if byte == "\x01":
+        print "Filter: 1"
+    elif byte == "\x02":
+        print "Filter: 2"
+    elif byte == "\x03":
+        print "Filter: 3"
+
+
+def ic7410_show_frequency(cmd):
+    cmd = ''.join(cmd).encode('hex')
+    print cmd[10:11]+"."+cmd[11:12]+cmd[8:9]+cmd[9:10] + "." + cmd[6:7]+ cmd[7:8] + cmd[4:5] + "." + cmd[5:6]+ cmd[2:3]  + "Ghz"
+
+def ic7410_read():
     s = ""
-    while s != "fe":
-        if debug : print "waiting for sync...." + s
-        s = ser.read().encode('hex')
-    if ser.read().encode('hex') == "fe":
+    while s != "\xfe":
+        if debug : print "waiting for sync...." + s.encode('hex')
+        s = ser.read()
+    if ser.read() == "\xfe":
         if debug : print "synced, packet info :"
         i = 0
-        command = ""
-        while s != "fd":
-            s = ser.read().encode('hex')
+        command = []
+        while s != "\xfd":
+            s = ser.read()
             if  i == 0 :
-                if debug : print "TO:"+s
+                if debug : print "TO:"+ s.encode('hex')
             elif i == 1:
-                if debug : print "FROM:"+s
+                if debug : print "FROM:"+ s.encode('hex')
             else:
-                command = command + s
+                command.append(s)
             i +=1
-        return command[:-2]
+        command.pop()
+        return command
 
 read = 1
 ic7410_get_frequency()
 while read!=0:
-    cmd = read_7410()
-    if cmd == "010201":
-       print "Filter 1 AM"
-    elif cmd == "010202":
-       print "Filter 2 AM"
-    elif cmd == "010203":
-       print "Filter 3 AM"
-    elif cmd[:2] == "03":
-       print cmd[10:11]+"."+cmd[11:12]+cmd[8:9]+cmd[9:10] + "." + cmd[6:7]+ cmd[7:8] + cmd[4:5] + "." + cmd[5:6]+ cmd[2:3]  + "Ghz"
-    elif cmd[:2] == "00":
-       print cmd[10:11]+"."+cmd[11:12]+cmd[8:9]+cmd[9:10] + "." + cmd[6:7]+ cmd[7:8] + cmd[4:5] + "." + cmd[5:6]+ cmd[2:3]  + "Ghz"
+    cmd = ic7410_read()
+    if cmd[0] == "\x00":
+        ic7410_show_frequency(cmd)
+    elif cmd[0] == "\x01":
+        ic7410_show_mode(cmd[1])
+        ic7410_show_filter(cmd[2])
+    elif cmd[0] == "\x03":
+        ic7410_show_frequency(cmd)
     else:
-       print "Unknown Command (" +cmd+ "), mail blendz@shaw.ca"
+       print "Unknown Command (" +cmd[0].encode('hex')+ "), mail blendz@shaw.ca"
 ser.close()
 
