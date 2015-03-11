18dde5f9415b358f8be7acdaf63e5620107c10b7
MrBoogs <blendz@shaw.ca>
2015-03-10 21:46:56 -0600

    More updates  without good commit messages

--- picom.py
+++ picom.py
@@ -6,6 +6,15 @@ ser = serial.Serial('/dev/ttyUSB0', 19200)
 
 def ic7410_get_frequency():
     s = ser.write("\xfe\xfe\x80\x00\x03\xfd")
+    
+def ic7410_get_band_edge():
+    s = ser.write("\xfe\xfe\x80\x00\x02\xfd")
+
+def ic7410_get_mode_filter():
+    s = ser.write("\xfe\xfe\x80\x00\x04\xfd")
+
+def ic7410_set_frequency():
+    s = ser.write("\xfe\xfe\x80\x00\x05\xfd")
 
 def ic7410_show_mode(byte):
     if byte == "\x00":
@@ -35,10 +44,14 @@ def ic7410_show_filter(byte):
     elif byte == "\x03":
         print "Filter: 3"
 
-
 def ic7410_show_frequency(cmd):
     cmd = ''.join(cmd).encode('hex')
-    print cmd[10:11]+"."+cmd[11:12]+cmd[8:9]+cmd[9:10] + "." + cmd[6:7]+ cmd[7:8] + cmd[4:5] + "." + cmd[5:6]+ cmd[2:3]  + "Ghz"
+    print "Frequency:" + cmd[10:11]+"."+cmd[11:12]+cmd[8:9]+cmd[9:10] + "." + cmd[6:7]+ cmd[7:8] + cmd[4:5] + "." + cmd[5:6]+ cmd[2:3]
+
+def ic7410_show_band_edge(cmd):
+    cmd = ''.join(cmd).encode('hex')
+    print "Band Start:" + cmd[10:11]+"."+cmd[11:12]+cmd[8:9]+cmd[9:10] + "." + cmd[6:7]+ cmd[7:8] + cmd[4:5] + "." + cmd[5:6]+ cmd[2:3] 
+    print "Band End:" + cmd[22:23]+"."+cmd[23:24]+cmd[20:21]+cmd[21:22] + "." + cmd[18:19]+ cmd[19:20] + cmd[16:17] + "." + cmd[17:18]+ cmd[14:15] 
 
 def ic7410_read():
     s = ""
@@ -62,17 +75,26 @@ def ic7410_read():
         return command
 
 read = 1
-ic7410_get_frequency()
+#ic7410_get_band_edge()
+#ic7410_get_frequency()
+#ic7410_get_mode_filter()
 while read!=0:
     cmd = ic7410_read()
-    if cmd[0] == "\x00":
-        ic7410_show_frequency(cmd)
-    elif cmd[0] == "\x01":
-        ic7410_show_mode(cmd[1])
-        ic7410_show_filter(cmd[2])
-    elif cmd[0] == "\x03":
-        ic7410_show_frequency(cmd)
-    else:
-       print "Unknown Command (" +cmd[0].encode('hex')+ "), mail blendz@shaw.ca"
+    if len(cmd) > 2:
+        if cmd[0] == "\x00":
+            ic7410_show_frequency(cmd)
+        elif cmd[0] == "\x01":
+            ic7410_show_mode(cmd[1])
+            ic7410_show_filter(cmd[2])
+        elif cmd[0] == "\x02":
+            ic7410_show_band_edge(cmd)
+        elif cmd[0] == "\x03":
+            ic7410_show_frequency(cmd)
+        elif cmd[0] == "\x04":
+            ic7410_show_mode(cmd[1])
+            ic7410_show_filter(cmd[2])
+        else:
+            print "Unknown Command (" +cmd[0].encode('hex')+ "), mail blendz@shaw.ca"
+            print cmd
 ser.close()
 
